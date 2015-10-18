63cbb7e1c3f75c67239948065e95d9972f9a3db4
jeffchao98 <philadelphiafs@gmail.com>
2015-10-18 22:21:49 +0900

    [Update]Selection Sort
    1.Add Selection sort function in Sort module

--- WhiteBoard/DataStructure/Sort.py
+++ WhiteBoard/DataStructure/Sort.py
@@ -14,3 +14,18 @@ class Sort(object):
                     else:
                         #print("===>>>"+str(list_input))
                         break
+    #Selection Sort : Input Type - array, No Output and we will change the content of the input
+    def SelectionSort(self, list_input):
+        i_lenlist = len(list_input)
+        if i_lenlist>1:
+            for each_id in range(i_lenlist):
+                i_minId = each_id
+                #We will scan in a range [k, input length], k is between in 0 and input length
+                for each_sub in range(each_id,i_lenlist):
+                    if list_input[i_minId]>list_input[each_sub]:
+                        i_minId = each_sub
+                #The lowest position will determined in the loop
+                #Then we switch the value of the position with the head position of the sub range
+                i_tmp = list_input[each_id]
+                list_input[each_id] = list_input[i_minId]
+                list_input[i_minId] = i_tmp
--- WhiteBoard/__init__.py
+++ WhiteBoard/__init__.py
@@ -2,6 +2,7 @@ import DataStructure.Sort
 
 mSort_test = DataStructure.Sort.Sort()
 list_test = [1,9,5,6,3,7,0,2,5,9,8]
-mSort_test.InsertSort(list_test)
 print(list_test)
-print(1)
+print(sorted(list_test))
+mSort_test.SelectionSort(list_test)
+print(list_test)
